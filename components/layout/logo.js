e6ca081b02295228b93d711ec7a10b74b1990d7f
Jony-Jas <jonyjasjonyjas@gmail.com>
2022-07-17 14:00:07 +0530

    logo change

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,7 +1,7 @@
 import classes from "./logo.module.css";
 
 function Logo() {
-  return <div className={classes.logo}>Jony&apos;s Blog</div>;
+  return <div className={classes.logo}>intoCode</div>;
 }
 
 export default Logo;
